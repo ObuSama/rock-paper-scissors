2a0cb0819eaab4f6563b9d9fd002ad420efb935d
obusama <eswaruce@gmail.com>
2022-10-30 19:12:13 +0530

    init

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,14 +1,21 @@
+from itertools import count
 import random
 
 def play():
-    user = input(" Lets play Rock-Paper-Scissors. Choose one --'r' for rock, 'p' for paper, 's' for scissors :")
+    user = input(" Choose one --'r' for rock, 'p' for paper, 's' for scissors :")
     computer = random.choice(['r','p','s'])
     print(f"I have chosen {computer}")
+    return (user, computer)
+def print_result(user,computer):    
     if user == computer:
-        return 'It\'s a tie!'
-    if is_win(user,computer):
-        return 'You Won!'
-    return 'You lost!'
+        print('It\'s a tie!')
+        return(0,0)
+    elif is_win(user,computer):
+        print( 'You Won!')
+        return(1,0)
+    else:
+        print('You lost!')
+        return(0,1)
 def is_win(player,opponent):
 
     if (player=='p' and opponent =='s') or (player=='r' and opponent =='p') or (player=='s' and opponent =='r'):
@@ -16,5 +23,21 @@ def is_win(player,opponent):
     return True
 
 if __name__ =="__main__":
-    print(play())
-    
+    best_of = int(input("Lets play Rock-Paper-Scissors. Choose the number of games in set, best of :"))
+    player = 0
+    computer = 0
+    games = 0
+    while games< best_of:
+        games+=1
+        print(f"Game: {games}")
+        user, computer = play()
+        user_score,computer_score = print_result(user,computer)
+        player+= user_score
+        computer+=computer_score
+    if player==computer:
+        print("The set is tied")
+    elif  player>computer:
+        print("You won the set!")
+    else:
+        print("You lost the set!")
+        
