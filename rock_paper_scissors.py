688b66a9d032b77e97bf07499896e79caa365cc1
obusama <eswaruce@gmail.com>
2022-10-30 19:19:14 +0530

    fixed bugs

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -24,19 +24,20 @@ def is_win(player,opponent):
 
 if __name__ =="__main__":
     best_of = int(input("Lets play Rock-Paper-Scissors. Choose the number of games in set, best of :"))
-    player = 0
-    computer = 0
+    player_total = 0
+    computer_total = 0
     games = 0
     while games< best_of:
         games+=1
         print(f"Game: {games}")
         user, computer = play()
         user_score,computer_score = print_result(user,computer)
-        player+= user_score
-        computer+=computer_score
-    if player==computer:
+        player_total += user_score
+        computer_total += computer_score
+    print(f"Score in the set -- You:{player_total}, Computer: {computer_total}")
+    if player_total==computer_total:
         print("The set is tied")
-    elif  player>computer:
+    elif  player_total>computer_total:
         print("You won the set!")
     else:
         print("You lost the set!")
